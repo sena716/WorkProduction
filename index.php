1b05f327021783903ca0ce6a62a01e94744d109e
19s020kr <19s020kr@gmail.com>
2020-08-21 13:58:23 +0900

    header作成中

--- index.php
+++ index.php
@@ -8,22 +8,24 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <section class="header">
+
+        <p class="header--teamLogo">
+            <img src="common/images/teamlogo.PNG" alt="チームロゴ画像">
+        </p>
+
+        <ul class="header--pageLink">
+            <li><a href="../map/map.html">地図から探す</a></li>
+            <li><a href="../photo/photo.html">写真から探す</a></li>
+            <li><a href="../conditions/conditions.html">条件から探す</a></li>
+            <li><a href="../name/name.html">名前から探す</a></li>
+            <li><a href="../ranking/ranking.html">ランキング</a></li>
+        </ul>
+
+    </section>
+
     <h1>作品制作</h1>
-
-    <p>
-        <img src="common/images/teamlogo.PNG" alt="チームロゴ画像">
-    </p>
-
-    <ul>
-        <li><a href="../map/map.html">地図から探す</a></li>
-        <li><a href="../photo/photo.html">写真から探す</a></li>
-        <li><a href="../conditions/conditions.html">条件から探す</a></li>
-        <li><a href="../name/name.html">名前から探す</a></li>
-        <li><a href="../ranking/ranking.html">ランキング</a></li>
-    </ul>
-
-
+    
     <script src="js/plugin/jquery-3.5.1.min.js"></script>
     <script src="common/js/main.js"></script>
     <script src="js/index/main.js"></script>
