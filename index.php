148d3b702bf914527eadb45f8e5e11b8fd63d8a2
冨山　聖那 <19s020kr@gmail.com>
2020-08-30 11:21:13 +0900

    topページ　作成中

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="common/css/style.css">
     <link rel="stylesheet" href="common/css/fontawesome/css/all.min.css">
     <link rel="stylesheet" href="css/index/style.css">
-    <title>titlepage</title>
+    <title>絶滅危惧種図鑑</title>
 </head>
 <body>
     <section class="top">
@@ -21,46 +21,42 @@
             </h1>
 
             <div class="top--main__pageLink">
-            <ul>
-                <li>
-                    <a href="map/map.html">
-                        <i class="fas fa-map-marked-alt"></i>
-                        <p>地図から探す</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="photo/photo.html">
-                        <i class="fas fa-camera"></i>
-                        <p>写真から探す</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="conditions/conditions.html">
-                        <i class="fas fa-search-plus"></i>
-                        <p>条件から探す</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="name/name.html">
-                        <i class="fas fa-signature"></i>
-                        <p>名前から探す</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="ranking/ranking.html">
-                        <i class="fas fa-crown"></i>
-                        <p>ランキング</p>
-                    </a>
-                </li>
-            </ul>
+                <ul>
+                    <li>
+                        <a href="map/map.html">
+                            <i class="fas fa-map-marked-alt"></i>
+                            <p>地図から探す</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="photo/photo.html">
+                            <i class="fas fa-camera"></i>
+                            <p>写真から探す</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="conditions/conditions.html">
+                            <i class="fas fa-search-plus"></i>
+                            <p>条件から探す</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="name/name.html">
+                            <i class="fas fa-signature"></i>
+                            <p>名前から探す</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="ranking/ranking.html">
+                            <i class="fas fa-crown"></i>
+                            <p>ランキング</p>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
 
-        </div>
-
-
-
-        <section class="top--animal">
-        
+        <div class="top--animal">
             <p class="top--animal__tori">
                 <img src="common/images/tori.svg" alt="鳥">
             </p>
@@ -70,13 +66,10 @@
             <p class="top--animal__kame">
                 <img src="common/images/kame.svg" alt="亀">
             </p>
-
-         </section>
+        </div>
 
     </section>
 
-
-    
     <script src="js/plugin/jquery-3.5.1.min.js"></script>
     <script src="common/js/main.js"></script>
     <script src="js/index/main.js"></script>
