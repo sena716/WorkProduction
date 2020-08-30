cc8024c455909d070b645d3a8e629c1f666fb6ff
冨山　聖那 <19s020kr@gmail.com>
2020-08-30 10:20:16 +0900

    topページ　作成中

--- index.php
+++ index.php
@@ -15,9 +15,11 @@
             <img src="common/images/teamLogo.png" alt="チームロゴ">
         </p>
         
-        <h1 class="top--titleLogo">
-            <img src="common/images/titlepcLogo.svg" alt="絶滅危惧種図鑑">
-        </h1>
+        <div class="top--titleLogo">
+            <h1>
+                <img src="common/images/titlepcLogo.svg" alt="絶滅危惧種図鑑">
+            </h1>
+        </div>
 
         <div class="top--pageLink">
             <ul>
