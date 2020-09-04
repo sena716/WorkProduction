7005128ea9fe5da25460f133b924ed669346491e
sena716 <19s020kr@gmail.com>
2020-09-04 10:31:25 +0900

    js変更

--- js/index/main.js
+++ js/index/main.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-//マウスオーバー時の処理
-function mouseOn(){
-    var obj = document.getElementById("bear_hand");
-    obj.src = "img/くまからだ全体.svg";
-  }
-   
-  //マウスアウト時の処理
-  function mouseOff(){
-    var obj = document.getElementById("bear_hand");
-    obj.src = "img/くまからだ全体差分.svg";
-  }
-  
-  //マウスオーバー時の処理
-  function mouseOn_mini(){
-    var obj = document.getElementById("mini_bear_hand");
-    obj.src = "img/くまからだ全体.svg";
-  }
-   
-  //マウスアウト時の処理
-  function mouseOff_mini(){
-    var obj = document.getElementById("mini_bear_hand");
-    obj.src = "img/くまからだ全体差分.svg";
-  }
-=======
 //マウスオーバー時の処理
 function mouseOn(){
     var obj = document.getElementById("bear_hand");
@@ -47,4 +22,3 @@ function mouseOff(){
 //     obj.src = "../../common/images/after.svg";
 // }
   
->>>>>>> 673f771eead85afa930777100d07ed734c1f6855
