f49c85bf56b93b0262ddcc1706f9536257606a5b
sena716 <19s020kr@gmail.com>
2020-09-04 11:49:02 +0900

    rakko変更

--- js/index/main.js
+++ js/index/main.js
@@ -21,6 +21,17 @@ function mouseOff_minikuma(){
     obj.src = "../../common/images/kuma_after.svg";
 }
 
+img = new Array("common/images/rakko.svg","common/images/kuma_before.svg");
+count = -1;
+ingTimer();
+
+function imgTimer(){
+    count++;
+    if(count == img.length) count = 0;
+    document.rakko_.src = img[count];
+    serTimeout("imgTimer()",1000);
+}
+
 // //マウスオーバー時の処理
 // function mouseOn_mini(){
 //     var obj = document.getElementById("mini_bear_hand");
