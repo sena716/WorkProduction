7c1789776fb7765cbf4d9a80281bb8fc56a94d9a
arimura <19s001shun@gmail.com>
2020-09-04 09:40:28 +0900

    micro cms 接続中

---
+++ js/map/main.js
@@ -0,0 +1,26 @@
+// micro cms 設定
+fetch("https://cardezasample.microcms.io/api/v1/sample", {
+headers: {
+    "X-API-KEY": "0360e61a-c834-46ec-b5dd-2842c877d1e0"
+}
+})
+.then(res => res.json())
+.then(json => {
+    document.getElementById("name").innerHTML = json.name;
+})
+
+
+
+// -1-1-1
+// -東西-地方-県
+
+// 北海道地方 click イベント
+function hokkaidouAllCheck(){
+    if( $('input[name="map-1-1"]').prop('checked')){
+        $('input[name="map-1-1-1"]').prop('checked', true );    
+    } else {
+        $('[name="map-1-1-1"]').prop('checked', false );
+    }
+}
+
+// 
