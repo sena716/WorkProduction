5f7e4a176c874aa3c3859d1431dcba13c8944d9d
sena716 <19s020kr@gmail.com>
2020-09-04 09:17:57 +0900

    index修正

--- index.php
+++ index.php
@@ -15,10 +15,12 @@
         <p class="top--teamLogo">
             <img src="common/images/teamLogo.png" alt="チームロゴ">
         </p>
+
         <div class="top--main">
             <h1 class="top--main__titleLogo">
                 <img src="common/images/titlepcLogo.svg" alt="絶滅危惧種図鑑">
             </h1>
+
             <div class="top--main__pageLink">
                 <ul>
                     <li>
@@ -56,47 +58,17 @@
         </div>
 
         <div class="top--animal">
-                <!-- <div class="top--animal__bear">
-                    <img id="bear_hand" src="img/くまからだ全体.svg" onmouseover="mouseOn();" onmouseout="mouseOff();"/>
-                </div>
-                <div class="top--animal__mini_bear">
-                    <img id="mini_bear_hand" src="img/くまからだ全体.svg" onmouseover="mouseOn_mini();" onmouseout="mouseOff_mini();"/>
-                </div>
-            <div class="top--animal__top_rogo">
-                <div class="top--animal__top_rogo__rogo">
-                    <img src="img/PCロゴ.svg" alt="ロゴ">
-                </div>
-            </div>
-        <div class ="top--animal__bard">
-            <img src="img/鳥.svg" alt="鳥">
+            <p class="top--animal__tori">
+                <img src="common/images/tori.svg" alt="鳥">
+            </p>
+            <p class="top--animal__kuma">
+                <img src="common/images/kuma1.svg" alt="熊">
+            </p>
+            <p class="top--animal__kame">
+                <img src="common/images/kame.svg" alt="亀">
+            </p>
         </div>
-        <div class="top--animal__css_work_farst">
-            <div class="top--animal__css_work_farst__css_work_last">
-                <img src="img/亀差分.svg" alt="亀歩き（終）">
-            </div>
-            </div> -->
-            <div class ="image_animal">
-  <div class="bear">
-    <img id="bear_hand" src="common/images/kame.svg" onmouseover="mouseOn();" onmouseout="mouseOff();"/>
-  </div>
-  <div class="mini_bear">
-    <img id="mini_bear_hand" src="img/くまからだ全体.svg" onmouseover="mouseOn_mini();" onmouseout="mouseOff_mini();"/>
-  </div>
-  <div class="top_rogo">
-  <div class="rogo">
-    <img src="img/PCロゴ.svg" alt="ロゴ">
-  </div>
-</div>
-<div class ="bard">
-  <img src="img/鳥.svg" alt="ロゴ">
-</div>
-  <div class="css_work_farst">
-    <div class="css_work_last">
-      <img src="img/亀差分.svg" alt="亀歩き（終）">
-    </div>
-  </div>
-</div>
-        </div>
+
     </section>
 
     <script src="js/plugin/jquery-3.5.1.min.js"></script>
