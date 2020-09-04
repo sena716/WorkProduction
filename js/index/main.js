ca6d9fde6710471ebb60ec269cb2025c734fa61d
sena716 <19s020kr@gmail.com>
2020-09-04 09:24:52 +0900

    js変更

--- js/index/main.js
+++ js/index/main.js
@@ -1,23 +1,23 @@
-//マウスオーバー時の処理
-function mouseOn(){
-    var obj = document.getElementById("bear_hand");
-    obj.src = "img/くまからだ全体.svg";
-  }
+// //マウスオーバー時の処理
+// function mouseOn(){
+//     var obj = document.getElementById("bear_hand");
+//     obj.src = "img/くまからだ全体.svg";
+//   }
    
-  //マウスアウト時の処理
-  function mouseOff(){
-    var obj = document.getElementById("bear_hand");
-    obj.src = "img/くまからだ全体差分.svg";
-  }
+//   //マウスアウト時の処理
+//   function mouseOff(){
+//     var obj = document.getElementById("bear_hand");
+//     obj.src = "img/くまからだ全体差分.svg";
+//   }
   
-  //マウスオーバー時の処理
-  function mouseOn_mini(){
-    var obj = document.getElementById("mini_bear_hand");
-    obj.src = "img/くまからだ全体.svg";
-  }
+//   //マウスオーバー時の処理
+//   function mouseOn_mini(){
+//     var obj = document.getElementById("mini_bear_hand");
+//     obj.src = "img/くまからだ全体.svg";
+//   }
    
-  //マウスアウト時の処理
-  function mouseOff_mini(){
-    var obj = document.getElementById("mini_bear_hand");
-    obj.src = "img/くまからだ全体差分.svg";
-  }
+//   //マウスアウト時の処理
+//   function mouseOff_mini(){
+//     var obj = document.getElementById("mini_bear_hand");
+//     obj.src = "img/くまからだ全体差分.svg";
+//   }
