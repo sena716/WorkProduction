ff0c47ed2b8e2f7210a29414a34ed4dff0880aaa
19s020kr <19s020kr@gmail.com>
2020-11-10 10:00:33 +0900

    page追加

---
+++ pages/photo.vue
@@ -0,0 +1,162 @@
+<template>
+    <div id="app">
+        <div class="Toggle">
+    　　<span></span>
+    　　<span></span>
+    　　<span></span>
+    </div>
+
+    <section class="header">
+
+        <p class="header--titleLogo">
+            <a href="../index.php"><img src="../common/images/titlepcLogo.svg" alt="絶滅危惧種図鑑"></a>
+        </p>
+
+        <ul class="header--pageLink">
+            <li><a href="../map/map.html">地図から探す</a></li>
+            <li><a href="photo.html">写真から探す</a></li>
+            <li><a href="../conditions/conditions.html">条件から探す</a></li>
+            <li><a href="../name/name.html">名前から探す</a></li>
+            <li><a href="../ranking/ranking.html">ランキング</a></li>
+        </ul>
+
+    </section>
+
+    <section class="main">
+        <h1>写真から探す</h1>
+
+        <ul class="main__each">
+            <li>
+                <a href="">
+                    <img src="../common/images/photo/photoSample.jpg" alt="うさぎ">
+                    <p>うさぎ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../common/images/photo/photoSample.jpg" alt="うさぎ">
+                    <p>うさぎ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../common/images/photo/photoSample.jpg" alt="うさぎ">
+                    <p>うさぎ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../common/images/photo/photoSample.jpg" alt="うさぎ">
+                    <p>うさぎ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../common/images/photo/photoSample.jpg" alt="うさぎ">
+                    <p>うさぎ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../common/images/photo/photoSample.jpg" alt="うさぎ">
+                    <p>うさぎ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../common/images/photo/photoSample.jpg" alt="うさぎ">
+                    <p>うさぎ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../common/images/photo/photoSample.jpg" alt="うさぎ">
+                    <p>うさぎ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../common/images/photo/photoSample.jpg" alt="うさぎ">
+                    <p>うさぎ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../common/images/photo/photoSample.jpg" alt="うさぎ">
+                    <p>うさぎ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../common/images/photo/photoSample.jpg" alt="うさぎ">
+                    <p>うさぎ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../common/images/photo/photoSample.jpg" alt="うさぎ">
+                    <p>うさぎ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../common/images/photo/photoSample.jpg" alt="うさぎ">
+                    <p>うさぎ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../common/images/photo/photoSample.jpg" alt="うさぎ">
+                    <p>うさぎ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../common/images/photo/photoSample.jpg" alt="うさぎ">
+                    <p>うさぎ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../common/images/photo/photoSample.jpg" alt="うさぎ">
+                    <p>うさぎ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../common/images/photo/photoSample.jpg" alt="うさぎ">
+                    <p>うさぎ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../common/images/photo/photoSample.jpg" alt="うさぎ">
+                    <p>うさぎ</p>
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    <img src="../common/images/photo/photoSample.jpg" alt="うさぎ">
+                    <p>うさぎ</p>
+                </a>
+            </li>
+        </ul>
+
+    </section>
+    </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  data: function() {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+
+</script>
+    <script src="../js/plugin/jquery-3.5.1.min.js"></script>
+    <script src="../common/js/main.js"></script>
+    <script src="../js/photo/main.js"></script>
