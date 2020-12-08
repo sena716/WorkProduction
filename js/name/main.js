e469a651586e93fcbf85d7695bdf1c234bdd45b3
2020 DSP4101 <2020DSP4101@ntsv6101.com>
2020-12-08 14:41:01 +0900

    頭文字の取得作業を中断

--- js/name/main.js
+++ js/name/main.js
@@ -1,39 +1,32 @@
-function nameSearchA(){
-
+// CMS接続
+function InitialSearch(){
+    console.log("a");
 }
-function nameSearchKa(){
+function nameSearch (Initial){
     
+    fetch("https://workproduction.microcms.io/api/v1/date?limit=30", {
+        headers: {
+            "X-API-KEY": "c2aa018e-aeb0-43c6-b6c2-e2bc343ab15e"
+        }
+    })
+    .then(res => res.json())
+    .then(json => {
+        contents = json.contents;
+        console.log(contents[0].name);
+        for(let i  = 0;i <contents.length;i++){
+            console.log(contents[i].name);
+            InitialSearch();
+        }
+        /*$('.main').empty();
+        $('.main').append('<h1>検索結果</h1>');
+        $('.main').append('<ul class="main__each"></ul>');
+        */
+        // for( let i = 0; contents.length > i; i++ ){
+        //     for( let j = 0; checkI.length > j; j++ ){
+        //         if( contents[i].name == checkI[j] ){
+        //             $('.main__each').append('<li><img src="'+ contents[i].img.url +'"><p>'+ contents[i].name +'</p></li>');
+        //         }
+        //     }
+        // }
+    });
 }
-function nameSearchSa(){
-    
-}
-function nameSearchTa(){
-    
-}
-function nameSearchNa(){
-    
-}
-function nameSearchHa(){
-    
-}
-function nameSearchMa(){
-    
-}
-function nameSearchYa(){
-    
-}
-function nameSearchRa(){
-    
-}
-function nameSearchWa(){
-    
-}
-
-$('.lineSelect').click(function(){
-    $('.main__line').addClass("active");
-    $('.main__search').removeClass("active");
-});
-$('.lineSearch').click(function(){
-    $('.main__search').addClass("active");
-    $('.main__line').removeClass("active");
-});
