d1a7fc4b6ba502479853ee4db42a1eac5e244874
2020 DSP6101 <2020DSP6101@ntsv6101.com>
2020-12-11 12:25:51 +0900

    動物名並び替え途中

--- js/name/main.js
+++ js/name/main.js
@@ -1,6 +1,25 @@
+// $(function(){
+//     var lists = [
+//         {name: 'メロン', price: 500},
+//         {name: 'バナナ', price: 100},
+//         {name: 'スイカ', price: 280},
+//         {name: 'イチゴ', price: 300}
+//     ];
+//     lists.sort(function(a, b) {
+//         console.log( a,b );
+//         if (a.name > b.name) {
+//             return 1;
+//         } else {
+//             return -1;
+//         }
+//     })
+    
+//     console.log(lists);
+// });
+
 // CMS接続
 function InitialSearch(){
-    console.log("a");
+    
 }
 function nameSearch (Initial){
     
@@ -12,11 +31,28 @@ function nameSearch (Initial){
     .then(res => res.json())
     .then(json => {
         contents = json.contents;
-        console.log(contents[0].name);
         for(let i  = 0;i <contents.length;i++){
-            console.log(contents[i].name);
             InitialSearch();
-        }
+            // 動物の名前をstrに代入
+            var str= contents[i].name;
+            // strから名前の頭文字
+            var a =  str.slice(0,1);
+            // 並び替え途中
+            var list = [
+                { str },
+                
+            ];
+
+            console.log( list );
+            list.sort(function(a, b) {
+                    if (a.str > b.str) {
+                        return 1;
+                    } else {
+                        return -1;
+                    }
+                })
+            }
+
         /*$('.main').empty();
         $('.main').append('<h1>検索結果</h1>');
         $('.main').append('<ul class="main__each"></ul>');
