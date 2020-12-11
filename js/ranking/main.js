997c93cfe3283a31bb63e43d6a870c24a1a48b13
yukaaaaaaa <19s019yuka@gmail.com>
2020-12-11 09:40:32 +0900

    ランキング追加作業

--- js/ranking/main.js
+++ js/ranking/main.js
@@ -18,13 +18,13 @@ fetch("https://workproduction.microcms.io/api/v1/date?limit=30", {
             }
         }
     }
-    $('.main--rank__1').append('<img src="'+ contents[0].img.url +'"><p>'+ contents[0].name +'</p><p>'+ contents[0].bodyWeight +'</p>');
-    $('.main--rank__2').append('<img src="'+ contents[1].img.url +'"><p>'+ contents[1].name +'</p><p>'+ contents[1].bodyWeight +'</p>');
-    $('.main--rank__3').append('<img src="'+ contents[2].img.url +'"><p>'+ contents[2].name +'</p><p>'+ contents[2].bodyWeight +'</p>');
+    $('.main--rank__S1').append('<img src="'+ contents[0].img.url +'"><p>'+ contents[0].name +'</p><p>'+ contents[0].bodyWeight +'</p>');
+    $('.main--rank__S2').append('<img src="'+ contents[1].img.url +'"><p>'+ contents[1].name +'</p><p>'+ contents[1].bodyWeight +'</p>');
+    $('.main--rank__S3').append('<img src="'+ contents[2].img.url +'"><p>'+ contents[2].name +'</p><p>'+ contents[2].bodyWeight +'</p>');
 
-    $('.main--rankL__1').append('<img src="'+ contents[contents.length].img.url +'"><p>'+ contents[contents.length].name +'</p><p>'+ contents[contents.length].bodyWeight +'</p>');
-    $('.main--rankL__2').append('<img src="'+ contents[contents.length-1].img.url +'"><p>'+ contents[contents.length-1].name +'</p><p>'+ contents[contents.length-1].bodyWeight +'</p>');
-    $('.main--rankL__3').append('<img src="'+ contents[contents.length-2].img.url +'"><p>'+ contents[contents.length-2].name +'</p><p>'+ contents[contents.length-2].bodyWeight +'</p>');
+    $('.main--rank__L1').append('<img src="'+ contents[0].img.url +'"><p>'+ contents[0].name +'</p><p>'+ contents[0].bodyWeight +'</p>');
+    $('.main--rank__L2').append('<img src="'+ contents[0].img.url +'"><p>'+ contents[0].name +'</p><p>'+ contents[0].bodyWeight +'</p>');
+    $('.main--rank__L3').append('<img src="'+ contents[0].img.url +'"><p>'+ contents[0].name +'</p><p>'+ contents[0].bodyWeight +'</p>');
 
 });
 }
