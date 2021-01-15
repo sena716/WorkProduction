68917aca415d385f8d77cceeb8e6ff23cfaa1366
yuka <19s019yuka@gmail.com>
2021-01-15 14:56:08 +0900

    ア行抽出作業

--- js/name/main.js
+++ js/name/main.js
@@ -1,4 +1,5 @@
 // CMS接続
+
 function InitialSearch(){    
 }
 function nameSearch (Initial){
@@ -25,8 +26,8 @@ function nameSearch (Initial){
             if(b>a) return 1;
             return 0;
         });
-        for(let i  = 0;i <str.length;i++){
-            // console.log(str);
+        // for(let i  = 0;i <str.length;i++){
+        //     console.log(str.length);
             // //カタカナをひらがなに変更
             // var str = Initial;
             // hiraToKana(str); //アイウ
@@ -37,46 +38,89 @@ function nameSearch (Initial){
             //         return String.fromCharCode(chr);
             //     });
             // }
-        
+            let i = 0;
             var a = str[i].slice(0,1);
-            console.log(i);
             if(Initial== a ){
-                console.log(a);
+                // console.log(a);
             } else if( Initial === "あ" ) 
             {
-                // console.log('あ行');
-                // if (str[i].match(/^ア|^イ|^ウ|^エ|^オ/)) {
-                    aa = str[i].match(/^ア|^イ|^ウ|^エ|^オ/);
-                    // console.log(str[i]);
-                    console.log(aa);
+                //あいうえおのものだけを抽出
+                // if (str[i].match(/^ア|^イ|^ウ|^エ|^オ/)) 
+                // {                    
+                    for(let i  = 0;i <str.length;i++){
+                        
+                        for( let j = 0;j<contents.length;j++)
+                        {                           
+                            if(contents[j].name == str[i] )
+                            {
+                                if (str[i].match(/^ア/))
+                                {
+                                    $('.main__line--name').empty();
+                                    // $('.main__each').empty();
+                                    
+                                    $('.main').append('<ul class="main__each"></ul>');
+                                    $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p>'+ contents[j].name +'</p></li>');    
+                                    console.log(contents[j].name);
+                                    console.log(str[i]);
+                                    console.log('aaaa');
+                                }
+                                if (str[i].match(/^イ|^ウ|^エ|^オ/))
+                                {
+                                    $('.main__line--name').empty();
+                                    // $('.main__each').empty();
+                                    
+                                    $('.main').append('<ul class="main__line--name"></ul>');
+                                    $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p>'+ contents[j].name +'</p></li>');    
+                                    console.log(contents[j].name);
+                                    console.log(str[i]);
+                                    console.log('aaaa');
+                                }
+                            }
+                        }
+                    };                    
                 // }
-            } 
-            else if( Initial === "か" ) 
-            {
-                console.log('か行');
-            }
+            // } 
+            // else if( Initial === "か" ) 
+            // {
+            //     //あいうえおのものだけを抽出
+            //     if (str[i].match(/^カ|^キ|^ク|^ケ|^コ/)) 
+            //     {                    
+            //         for(let i  = 0;i <str.length;i++){
+                        
+            //             for( let j = 0;j<contents.length;j++)
+            //             {                           
+            //                 if(contents[j].name == str[i] )
+            //                 {
+            //                     if (str[i].match(/^カ|^キ|^ク|^ケ|^コ/))
+            //                     {
+            //                         $('.main__line--name').empty();
+                
+            //                         $('.main').append('<ul class="main__each"></ul>');
+            //                         $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p>'+ contents[j].name +'</p></li>');     
+
+            //                     }
+            //                 }
+            //             }
+            //         };                    
+            //     }
+            // }
         };
 
-        
-        $('.main__line--name').empty();
-        $('.main').append('<ul class="main__each"></ul>');
+        // $('.main__line--name').empty();
+        // $('.main').append('<ul class="main__each"></ul>');
 
-        //strのidとcontentsのidを照合
-        for(let i  = 0;i <str.length;i++){
+        // //strのidとcontentsのidを照合
+
+        // for(let i  = 0;i <str.length;i++){
             
-            for( let j = 0;j<contents.length;j++){
+        //     for( let j = 0;j<contents.length;j++){
                 
-                if(contents[j].name == str[i] ){
-                    
-                    
-                    $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p>'+ contents[j].name +'</p></li>');   
-                  
-                }
-                   
-                
-            }
+        //         if(contents[j].name == str[i] ){
+        //             $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p>'+ contents[j].name +'</p></li>');    
+        //         }
+        //     }
+        // };
         
-        };
         // for( let i = 0; contents.length > i; i++ ){
         //     for( let j = 0; checkI.length > j; j++ ){
         //         if( contents[i].name == checkI[j] ){
