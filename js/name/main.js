46b1aa6eedb48e41656137cef8c4e61befeb1ac7
19s020kr <19s020kr@gmail.com>
2021-01-24 23:06:46 +0900

    各ページ調整中

--- js/name/main.js
+++ js/name/main.js
@@ -1,9 +1,9 @@
 // CMS接続
 
-function InitialSearch(){    
+function InitialSearch(){
 }
 function nameSearch (Initial){
-    
+
     fetch("https://workproduction.microcms.io/api/v1/date?limit=30&fields=id,name,img", {
         headers: {
             "X-API-KEY": "c2aa018e-aeb0-43c6-b6c2-e2bc343ab15e"
@@ -12,13 +12,13 @@ function nameSearch (Initial){
     .then(res => res.json())
     .then(json => {
         contents = json.contents;
-        
+
         var str = [];
         for(let i  = 0;i <contents.length;i++){
             // InitialSearch();
             // 動物の名前をstrに代入
             str.push(contents[i].name);
-            
+
         }
         // 名前順に並び替え
         str.sort(function (a, b) {
@@ -42,55 +42,55 @@ function nameSearch (Initial){
             let i = 0;
             var a = str[i].slice(0,1);
             if(Initial== a ){
-                //あ行を抽出                  
+                //あ行を抽出
             } else if( Initial === "あ" ) //←ここを行ごとに変更する
             {
                 $('.main__each').remove();
-                
+
                 $('.main').append('<ul class="main__each"></ul>');
                 // $('.main__each').empty();
-                
+
                 for(let i  = 0;i <str.length;i++){
-                    
+
                     for( let j = 0;j<contents.length;j++)
-                    {                           
+                    {
                         if(contents[j].name == str[i] )
                         {
                             if (str[i].match(/^ア|^イ|^ウ|^エ|^オ/))//←ここを条件ごとに変更する
                             {
                                 //main__eachにcontentsを付与
-                                $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p>'+ contents[j].name +'</p></li>');    
+                                $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p class="animalName">'+ contents[j].name +'</p></li>');
                             }
                         }
                         }
-                    };   
+                    };
                 }
 
-                else if( Initial === "か" ) 
+                else if( Initial === "か" )
                 {
-                    
+
                     $('.main__each').remove();
 
                     $('.main').append('<ul class="main__each"></ul>');
 
                     for(let i  = 0;i <str.length;i++){
-                        
+
                         for( let j = 0;j<contents.length;j++)
-                        {                           
+                        {
                             if(contents[j].name == str[i] )
                             {
 
                                 if (str[i].match(/^カ|^キ|^ク|^ケ|^コ|^ガ|^ギ|^グ|^ゲ|^ゴ/))
                                 {
                                     //main__eachにcontentsを付与
-                                    $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p>'+ contents[j].name +'</p></li>');    
+                                    $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p class="animalName">'+ contents[j].name +'</p></li>');
                                 }
                             }
                         }
-                    };   
+                    };
                 }
 
-                else if( Initial === "さ" ) 
+                else if( Initial === "さ" )
                 {
 
                     $('.main__each').remove();
@@ -99,9 +99,9 @@ function nameSearch (Initial){
                     $('.main').append('<ul class="main__each"></ul>');
 
                     for(let i  = 0;i <str.length;i++){
-                        
+
                         for( let j = 0;j<contents.length;j++)
-                        {                           
+                        {
                             if(contents[j].name == str[i] )
                             {
 
@@ -109,14 +109,14 @@ function nameSearch (Initial){
                                 {
                                     // $('.main__each').empty();
                                     //main__eachにcontentsを付与
-                                    $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p>'+ contents[j].name +'</p></li>');    
+                                    $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p class="animalName">'+ contents[j].name +'</p></li>');
                                 }
                             }
                         }
-                    };   
+                    };
                 }
 
-                else if( Initial === "た" ) 
+                else if( Initial === "た" )
                 {
                     $('.main__each').remove();
 
@@ -124,25 +124,25 @@ function nameSearch (Initial){
                     $('.main').append('<ul class="main__each"></ul>');
 
                     for(let i  = 0;i <str.length;i++){
-                        
+
                         for( let j = 0;j<contents.length;j++)
-                        {                           
+                        {
                             if(contents[j].name == str[i] )
                             {
 
                                 if (str[i].match(/^タ|^チ|^ツ|^テ|^ト|^ダ|^ヂ|^ヅ|^デ|^ド/))
                                 {
-                                    
+
                                     // $('.main__each').empty();
                                     //main__eachにcontentsを付与
-                                    $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p>'+ contents[j].name +'</p></li>');    
+                                    $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p class="animalName">'+ contents[j].name +'</p></li>');
                                 }
                             }
                         }
-                    };   
+                    };
                 }
-                
-                else if( Initial === "な" ) 
+
+                else if( Initial === "な" )
                 {
                     $('.main__each').remove();
 
@@ -150,22 +150,22 @@ function nameSearch (Initial){
                     $('.main').append('<ul class="main__each"></ul>');
 
                     for(let i  = 0;i <str.length;i++){
-                        
+
                         for( let j = 0;j<contents.length;j++)
-                        {                           
+                        {
                             if(contents[j].name == str[i] )
                             {
 
                                 if (str[i].match(/^ナ|^ニ|^ヌ|^ネ|^ノ/))
                                 {
                                     //main__eachにcontentsを付与
-                                    $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p>'+ contents[j].name +'</p></li>');    
+                                    $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p class="animalName">'+ contents[j].name +'</p></li>');
                                 }
                             }
                         }
-                    };   
+                    };
                 }
-                else if( Initial === "は" ) 
+                else if( Initial === "は" )
                 {
 
                     $('.main__each').remove();
@@ -173,32 +173,32 @@ function nameSearch (Initial){
                     $('.main').append('<ul class="main__each"></ul>');
 
                     for(let i  = 0;i <str.length;i++){
-                        
+
                         for( let j = 0;j<contents.length;j++)
-                        {                           
+                        {
                             if(contents[j].name == str[i] )
                             {
 
                                 if (str[i].match(/^ハ|^ヒ|^フ|^ヘ|^ホ|^バ|^ビ|^ブ|^ベ|^ボ|^パ|^ピ|^プ|^ペ|^ポ/))
                                 {
                                     //main__eachにcontentsを付与
-                                    $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p>'+ contents[j].name +'</p></li>');    
+                                    $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p class="animalName">'+ contents[j].name +'</p></li>');
                                 }
                             }
                         }
-                    };   
+                    };
                 }
 
-                else if( Initial === "ま" ) 
+                else if( Initial === "ま" )
                 {
                     $('.main__each').remove();
 
                     $('.main').append('<ul class="main__each"></ul>');
 
                     for(let i  = 0;i <str.length;i++){
-                        
+
                         for( let j = 0;j<contents.length;j++)
-                        {                           
+                        {
                             if(contents[j].name == str[i] )
                             {
 
@@ -206,22 +206,22 @@ function nameSearch (Initial){
                                 {
                                     // $('.main__each').empty();
                                     //main__eachにcontentsを付与
-                                    $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p>'+ contents[j].name +'</p></li>');    
+                                    $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p class="animalName">'+ contents[j].name +'</p></li>');
                                 }
                             }
                         }
-                    };   
+                    };
                 }
 
-                else if( Initial === "や" ) 
+                else if( Initial === "や" )
                 {
                     $('.main__each').remove();
                     $('.main').append('<ul class="main__each"></ul>');
 
                     for(let i  = 0;i <str.length;i++){
-                        
+
                         for( let j = 0;j<contents.length;j++)
-                        {                           
+                        {
                             if(contents[j].name == str[i] )
                             {
 
@@ -229,22 +229,22 @@ function nameSearch (Initial){
                                 {
                                     // $('.main__each').empty();
                                     //main__eachにcontentsを付与
-                                    $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p>'+ contents[j].name +'</p></li>');    
+                                    $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p class="animalName">'+ contents[j].name +'</p></li>');
                                 }
                             }
                         }
-                    };   
+                    };
                 }
 
-                else if( Initial === "ら" ) 
+                else if( Initial === "ら" )
                 {
                     $('.main__each').remove();
                     $('.main').append('<ul class="main__each"></ul>');
-                    
+
                     for(let i  = 0;i <str.length;i++){
-                        
+
                         for( let j = 0;j<contents.length;j++)
-                        {                           
+                        {
                             if(contents[j].name == str[i] )
                             {
 
@@ -252,23 +252,23 @@ function nameSearch (Initial){
                                 {
                                     // $('.main__each').empty();
                                     //main__eachにcontentsを付与
-                                    $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p>'+ contents[j].name +'</p></li>');    
+                                    $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p class="animalName">'+ contents[j].name +'</p></li>');
                                 }
                             }
                         }
-                    };   
+                    };
                 }
 
-                else if( Initial === "わ" ) 
+                else if( Initial === "わ" )
                 {
 
                     $('.main__each').remove();
                     $('.main').append('<ul class="main__each"></ul>');
-                    
+
                     for(let i  = 0;i <str.length;i++){
-                        
+
                         for( let j = 0;j<contents.length;j++)
-                        {                           
+                        {
                             if(contents[j].name == str[i] )
                             {
 
@@ -276,13 +276,13 @@ function nameSearch (Initial){
                                 {
                                     // $('.main__each').empty();
                                     //main__eachにcontentsを付与
-                                    $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p>'+ contents[j].name +'</p></li>');    
+                                    $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p class="animalName">'+ contents[j].name +'</p></li>');
                                 }
                             }
                         }
-                    };   
-                }                              
-                
+                    };
+                }
+
 
         // $('.main__line--name').empty();
         // $('.main').append('<ul class="main__each"></ul>');
@@ -290,23 +290,29 @@ function nameSearch (Initial){
         // //strのidとcontentsのidを照合
 
         // for(let i  = 0;i <str.length;i++){
-            
+
         //     for( let j = 0;j<contents.length;j++){
-                
+
         //         if(contents[j].name == str[i] ){
-        //             $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p>'+ contents[j].name +'</p></li>');    
+        //             $('.main__each').append('<li><img src="'+ contents[j].img.url +'"><p class="animalName">'+ contents[j].name +'</p></li>');
         //         }
         //     }
         // };
-        
+
         // for( let i = 0; contents.length > i; i++ ){
         //     for( let j = 0; checkI.length > j; j++ ){
         //         if( contents[i].name == checkI[j] ){
-       
+
         //         }
         //     }
         // }
 
-      
+        $('.main__each li').on("click", function(){
+            let individualAnimal = $("p", this).html();
+
+            sessionStorage.setItem('animalInfo', individualAnimal);
+            location.href="/afterSearch/animalInfo/animalInfo.html";
+        });
+
     });
 }
--- js/ranking/main.js
+++ js/ranking/main.js
@@ -4,102 +4,73 @@ window.onload=function(){
         headers: {
             "X-API-KEY": "c2aa018e-aeb0-43c6-b6c2-e2bc343ab15e"
         }
-    }
-
-    // $('.main--rank__S1').style.backgroundImage = contents[0].img.url;
-    $('.main--rank__S1-img').attr('src', contents[0].img.url);
-    $('.main--rank__S1-info').append('<p>' + contents[0].name + '</p><p>'+ contents[0].bodyWeight +'</p>');
-    console.log( $('.main--rank__S1'));
-
-    $('.main--rank__S2-img').attr('src', contents[1].img.url);
-    $('.main--rank__S2-info').append('<p>' + contents[1].name + '</p><p>'+ contents[1].bodyWeight +'</p>');
-    console.log( $('.main--rank__S2'));
-
-    $('.main--rank__S3-img').attr('src', contents[2].img.url);
-    $('.main--rank__S3-info').append('<p>' + contents[2].name + '</p><p>'+ contents[2].bodyWeight +'</p>');
-    console.log( $('.main--rank__S3'));
-    // $('.main--rank__S1').append('<img src="'+ contents[0].img.url +'"><p>'+ contents[0].name +'</p><p>'+ contents[0].bodyWeight +'</p>');
-    // $('.main--rank__S2').append('<img src="'+ contents[1].img.url +'"><p class="comment">'+ contents[1].name +'</p><p class="comment">'+ contents[1].bodyWeight +'</p>');
-    // $('.main--rank__S3').append('<img src="'+ contents[2].img.url +'"><p class="comment">'+ contents[2].name +'</p><p class="comment">'+ contents[2].bodyWeight +'</p>');
     })
     .then(res => res.json())
     .then(json => {
+
         contents = json.contents;
-        let a;
+        let sortBox;
+        // let sample = [ 1, 3, 5, 2, 4 ];
+        // console.log( sample );
+        // for( let i = 0; sample.length > i; i++ ){
+        //     for( let j = 0; sample.length > j; j++ ){
+        //         if( sample[i] < sample[j] ){
+        //             sortBox = sample[i];
+        //             sample[i] = sample[j];
+        //             sample[j] = sortBox;
+        //         }
+        //     }
+        // }
+        // console.log( sample );
 
-
+        // const sortAraay = data.replace(/[^0-9.]/g, '');
+        let sortSampleI;
+        let sortSampleJ;
+        // 全長大ランキング
         //先頭の一文字しか参照していない
 
+        console.log ( contents[0],contents[1],contents[2] );
         for( let i = 0; contents.length > i; i++ ){
             for( let j = 0; contents.length > j; j++ ){
-                if( contents[i].bodyLength < contents[j].bodyLength )
-                {
-                    a=contents[i];
+                sortSampleI = contents[i].bodyLength.replace(/[^0-9.]/g, '');
+                sortSampleJ = contents[j].bodyLength.replace(/[^0-9.]/g, '');
+                if( sortSampleI < sortSampleJ ){
+                    sortBox = contents[i];
                     contents[i] = contents[j];
-                    contents[j] = a;
-
+                    contents[j] = sortBox;
                 }
             }
         }
-        for( let i = 0; contents.length > i; i++ ){
-            // if(contents[i].bodyLength.match(/[0-9]d{0,4}\/[a-z]+/i)){
+        console.log ( contents[0],contents[1],contents[2] );
 
-                console.log(contents[i].bodyLength);
-            // }
-            }
-        // const data = '[12.1,55-3,23～1]';
-        // const res = data.replace(/[^0-9.]/g, '');
-
-        // console.log(res); // 123
-
-        // console.log(contents);
-
-        // $('.main--rank__S1').style.backgroundImage = contents[0].img.url;
         $('.main--rank__S1-img').attr('src', contents[0].img.url);
         $('.main--rank__S1-info').append('<p>' + contents[0].name + '</p><p>'+ contents[0].bodyLength +'</p>');
-        console.log( $('.main--rank__S1'));
 
         $('.main--rank__S2-img').attr('src', contents[1].img.url);
         $('.main--rank__S2-info').append('<p>' + contents[1].name + '</p><p>'+ contents[1].bodyLength +'</p>');
-        console.log( $('.main--rank__S2'));
 
         $('.main--rank__S3-img').attr('src', contents[2].img.url);
         $('.main--rank__S3-info').append('<p>' + contents[2].name + '</p><p>'+ contents[2].bodyLength +'</p>');
-        console.log( $('.main--rank__S3'));
-
-        // $('.main--rank__S1').append('<img src="'+ contents[0].img.url +'"><p>'+ contents[0].name +'</p><p>'+ contents[0].bodyWeight +'</p>');
-        // $('.main--rank__S2').append('<img src="'+ contents[1].img.url +'"><p class="comment">'+ contents[1].name +'</p><p class="comment">'+ contents[1].bodyWeight +'</p>');
-        // $('.main--rank__S3').append('<img src="'+ contents[2].img.url +'"><p class="comment">'+ contents[2].name +'</p><p class="comment">'+ contents[2].bodyWeight +'</p>');
-
-        for( let i = 0; contents.length > i; i++ ){
-            for( let j = 0; contents.length > j; j++ ){
-                if( contents[i].bodyLength > contents[j].bodyLength ){
-                    a=contents[j];
-                    contents[j] = contents[i];
-                    contents[i] = a;
-                    // console.log(contents[j].bodyLength.length);
-                }
-            }
-        }
 
 
+        // 全長小ランキング
+        // for( let i = 0; contents.length > i; i++ ){
+        //     for( let j = 0; contents.length > j; j++ ){
+        //         if( contents[i].bodyLength > contents[j].bodyLength ){
+        //             a=contents[j];
+        //             contents[j] = contents[i];
+        //             contents[i] = a;
+        //         }
+        //     }
+        // }
 
+        // $('.main--rank__L1-img').attr('src', contents[0].img.url);
+        // $('.main--rank__L1-info').append('<p>' + contents[0].name + '</p><p>'+ contents[0].bodyLength +'</p>');
 
-        $('.main--rank__L1-img').attr('src', contents[0].img.url);
-        $('.main--rank__L1-info').append('<p>' + contents[0].name + '</p><p>'+ contents[0].bodyLength +'</p>');
-        console.log( $('.main--rank__L1'));
+        // $('.main--rank__L2-img').attr('src', contents[1].img.url);
+        // $('.main--rank__L2-info').append('<p>' + contents[1].name + '</p><p>'+ contents[1].bodyLength +'</p>');
 
-        $('.main--rank__L2-img').attr('src', contents[1].img.url);
-        $('.main--rank__L2-info').append('<p>' + contents[1].name + '</p><p>'+ contents[1].bodyLength +'</p>');
-        console.log( $('.main--rank__L2'));
-
-        $('.main--rank__L3-img').attr('src', contents[2].img.url);
-        $('.main--rank__L3-info').append('<p>' + contents[2].name + '</p><p>'+ contents[2].bodyLength +'</p>');
-        console.log( $('.main--rank__L3'));
-
-        // $('.main--rank__L1').append('<img src="'+ contents[0].img.url +'"><p>'+ contents[0].name +'</p><p>'+ contents[0].bodyWeight +'</p>');
-        // $('.main--rank__L2').append('<img src="'+ contents[1].img.url +'"><p>'+ contents[1].name +'</p><p>'+ contents[1].bodyWeight +'</p>');
-        // $('.main--rank__L3').append('<img src="'+ contents[2].img.url +'"><p>'+ contents[2].name +'</p><p>'+ contents[2].bodyWeight +'</p>');
-
+        // $('.main--rank__L3-img').attr('src', contents[2].img.url);
+        // $('.main--rank__L3-info').append('<p>' + contents[2].name + '</p><p>'+ contents[2].bodyLength +'</p>');
     });
-    }
+}
