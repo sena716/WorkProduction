d0786622166f0cbabed61e4a2c6b7fc1908ac986
19s020kr <19s020kr@gmail.com>
2021-01-29 02:06:52 +0900

    個体画面で複数県表示できるようにしました

--- js/ranking/main.js
+++ js/ranking/main.js
@@ -10,48 +10,34 @@ window.onload=function(){
 
         contents = json.contents;
         let sortBox;
-        // let sample = [ 1, 3, 5, 2, 4 ];
-        // console.log( sample );
-        // for( let i = 0; sample.length > i; i++ ){
-        //     for( let j = 0; sample.length > j; j++ ){
-        //         if( sample[i] < sample[j] ){
-        //             sortBox = sample[i];
-        //             sample[i] = sample[j];
-        //             sample[j] = sortBox;
-        //         }
-        //     }
-        // }
-        // console.log( sample );
-
-        // const sortAraay = data.replace(/[^0-9.]/g, '');
         let sortSampleI;
         let sortSampleJ;
         // 全長大ランキング
         //先頭の一文字しか参照していない
-        console.log( contents );
-        for( let i = 0; contents.length > i; i++ ){
-            for( let j = 0; contents.length > j; j++ ){
-                sortSampleI = contents[i].bodyLength.replace(/[^0-9.]/g, '');
-                sortSampleJ = contents[j].bodyLength.replace(/[^0-9.]/g, '');
-                console.log( sortSampleI, sortSampleJ );
-                if( sortSampleI < sortSampleJ ){
-                    console.log( contents[i].bodyLength, contents[j].bodyLength);
-                    sortBox = contents[i];
-                    contents[i] = contents[j];
-                    contents[j] = sortBox;
-                    console.log( contents[i].bodyLength, contents[j].bodyLength);
-                }
-            }
-        }
-        console.log( contents );
-        $('.main--rank__S1-img').attr('src', contents[0].img.url);
-        $('.main--rank__S1-info').append('<p>' + contents[0].name + '</p><p>'+ contents[0].bodyLength +'</p>');
+        // console.log( contents );
+        // for( let i = 0; contents.length > i; i++ ){
+        //     for( let j = 0; contents.length > j; j++ ){
+        //         sortSampleI = contents[i].bodyLength.replace(/[^0-9.]/g, '');
+        //         sortSampleJ = contents[j].bodyLength.replace(/[^0-9.]/g, '');
+        //         console.log( sortSampleI, sortSampleJ );
+        //         if( sortSampleI < sortSampleJ ){
+        //             console.log( contents[i].bodyLength, contents[j].bodyLength);
+        //             sortBox = contents[i];
+        //             contents[i] = contents[j];
+        //             contents[j] = sortBox;
+        //             console.log( contents[i].bodyLength, contents[j].bodyLength);
+        //         }
+        //     }
+        // }
+        // console.log( contents );
+        // $('.main--rank__S1-img').attr('src', contents[0].img.url);
+        // $('.main--rank__S1-info').append('<p>' + contents[0].name + '</p><p>'+ contents[0].bodyLength +'</p>');
 
-        $('.main--rank__S2-img').attr('src', contents[1].img.url);
-        $('.main--rank__S2-info').append('<p>' + contents[1].name + '</p><p>'+ contents[1].bodyLength +'</p>');
+        // $('.main--rank__S2-img').attr('src', contents[1].img.url);
+        // $('.main--rank__S2-info').append('<p>' + contents[1].name + '</p><p>'+ contents[1].bodyLength +'</p>');
 
-        $('.main--rank__S3-img').attr('src', contents[2].img.url);
-        $('.main--rank__S3-info').append('<p>' + contents[2].name + '</p><p>'+ contents[2].bodyLength +'</p>');
+        // $('.main--rank__S3-img').attr('src', contents[2].img.url);
+        // $('.main--rank__S3-info').append('<p>' + contents[2].name + '</p><p>'+ contents[2].bodyLength +'</p>');
 
 
         // 全長小ランキング
